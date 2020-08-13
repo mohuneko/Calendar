a8b730e73cd66cc6d71ddf215b98c36991208a6c
mohuneko <b0wdmohu@gmail.com>
2020-08-14 01:21:40 +0900

    addCalendarHead

--- js/main.js
+++ js/main.js
@@ -6,6 +6,33 @@ console.clear();
   const year = 2020;
   const month = 4; //5月
 
+  function getCalendarTail() {
+    const dates = [];
+
+    for (let i = 1; i < 7; i++) {
+      dates.push();
+    }
+  }
+
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      //30
+      //29, 30
+      //28, 29, 30
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
+
   function getCalendarBody() {
     const dates = []; //date:日付、 day:曜日
     const lastDate = new Date(year, month + 1, 0).getDate();
@@ -22,5 +49,6 @@ console.clear();
 
   }
 
-  getCalendarBody();
+  getCalendarHead();
+  // getCalendarBody();
 }
